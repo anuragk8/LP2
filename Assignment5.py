91c0503c9ba9785f98c3ac3eb0432e31199134e9
anuragk8 <65025894+anuragk8@users.noreply.github.com>
2022-05-17 22:47:31 +0530

    Add files via upload

--- Assignment5.py
+++ Assignment5.py
@@ -1,88 +1,68 @@
-from nltk.chat.util import Chat, reflections
+import random
 
-pairs = [
-    [
-        r"my name is (.*)",
-        ["Hello %1, How are you today ?",]
-    ],
-     [
-        r"what is your name ?",
-        ["My name is Chatty and I'm a chatbot ?",]
-    ],
-    [
-        r"how are you ?",
-        ["I'm doing good\nHow about You ?",]
-    ],
-    [
-        r"sorry (.*)",
-        ["Its alright","Its OK, never mind",]
-    ],
-    [
-        r"i'm (.*) doing good",
-        ["Nice to hear that","Alright :)",]
-    ],
-    [
-        r"hi|hey|hello",
-        ["Hello", "Hey there",]
-    ],
-    [
-        r"(.*) age?",
-        ["I'm a computer program dude\nSeriously you are asking me this?",]
+name = "Bot Number 286"
+monsoon = "rainy"
+food = ["pizza", "fries"]
+mood = "Smiley"
+resp = {
+    "what's your name?": [
+        "They call me {0}".format(name),
+        "I usually go by {0}".format(name),
+        "My name is the {0}".format(name)],
+    "what's today's weather?": [
+        "The weather is {0}".format(monsoon),
+        "It's {0} today".format(monsoon)],
+    "how are you?": [
+        "I am feeling {0}".format(mood),
+        "{0}! How about you?".format(mood),
+        "I am {0}! How about yourself?".format(mood), ],
+    "": ["Hey! Are you there?",
+         "What do you mean by these?",
+         ],
+    "what are the food options":
+        ["food {0}".format(food)],
+    "default": [
+        "This is a default message"]}
 
-    ],
-    [
-        r"what (.*) want ?",
-        ["Make me an offer I can't refuse",]
 
-    ],
-    [
-        r"(.*) created ?",
-        ["Vedant created me using Python's NLTK library ","top secret ;)",]
-    ],
-    [
-        r"(.*) (location|city) ?",
-        ['Chennai, Tamil Nadu',]
-    ],
-    [
-        r"how is weather in (.*)?",
-        ["Weather in %1 is awesome like always","Too hot man here in %1","Too cold man here in %1","Never even heard about %1"]
-    ],
-    [
-        r"i work in (.*)?",
-        ["%1 is an Amazing company, I have heard about it. But they are in huge loss these days.",]
-    ],
+def res(message):
+    if message in resp:
+        x = random.choice(resp[message])
+    else:
+        x = random.choice(resp["default"])
+    return x
 
-[
-        r"(.*)raining in (.*)",
-        ["No rain since last week here in %2","Damn its raining too much here in %2"]
-    ],
-    [
-        r"how (.*) health(.*)",
-        ["I'm a computer program, so I'm always healthy ",]
-    ],
-    [
-        r"(.*) (sports|game) ?",
-        ["I'm a very big fan of Football",]
-    ],
-    [
-        r"who (.*) sportsperson ?",
-        ["Messy","Ronaldo","Roony"]
-],
-    [
-        r"who (.*) (moviestar|actor)?",
-        ["Brad Pitt"]
-],
-    [
-        r"quit",
-        ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
 
-],
-]
+def real(xtext):
+    if "name" in xtext:
+        ytext = "what's your name?"
+    elif "monsoon" in xtext:
+        ytext = "what's today's weather?"
+    elif "how are" in xtext:
+        ytext = "how are you?"
+    elif "food" in xtext:
+        ytext = "what are the food options"
+    elif xtext == "":
+        ytext = ""
+    else:
+        ytext = "default"
+    return ytext
 
-def chatty():
-    print("Hi, I'm Chatty and I chat alot ;)\nPlease type lowercase English language to start a conversation. Type quit to leave ") #default message at the start
-    chat = Chat(pairs, reflections)
-    chat.converse()
 
-if __name__ == "__main__":
-    chatty()
+def send_message(message):
+    print((message))
+    response = res(message)
+    print((response))
+
+
+print("BOT: What is your name?")
+user_name = input()
+while 1:
+    my_input = input("Enter your message: ")
+    my_input = my_input.lower()
+    if my_input == "exit" or my_input == "stop":
+        break
+    related_text = real(my_input)
+    send_message(related_text)
+
+
