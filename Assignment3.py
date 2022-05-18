da54200ebaa942747c2d78c5e55f6a8f2c7b724f
anuragk8 <65025894+anuragk8@users.noreply.github.com>
2022-05-18 06:39:27 +0530

    Add files via upload

--- Assignment3.py
+++ Assignment3.py
@@ -1,82 +1,56 @@
-# A Python program for Prim's Minimum Spanning Tree (MST) algorithm.
-# The program is for adjacency matrix representation of the graph
+# Program to find the maximum profit
+# job sequence from a given array
+# of jobs with deadlines and profits
 
-import sys # Library for INT_MAX
-
-class Graph():
-
-	def __init__(self, vertices):
-		self.V = vertices
-		self.graph = [[0 for column in range(vertices)]
-					for row in range(vertices)]
-
-	# A utility function to print the constructed MST stored in parent[]
-	def printMST(self, parent):
-		print ("Edge \tWeight")
-		for i in range(1, self.V):
-			print (parent[i], "-", i, "\t", self.graph[i][parent[i]])
-
-	# A utility function to find the vertex with
-	# minimum distance value, from the set of vertices
-	# not yet included in shortest path tree
-	def minKey(self, key, mstSet):
-
-		# Initialize min value
-		min = sys.maxsize
-
-		for v in range(self.V):
-			if key[v] < min and mstSet[v] == False:
-				min = key[v]
-				min_index = v
-
-		return min_index
-
-	# Function to construct and print MST for a graph
-	# represented using adjacency matrix representation
-	def primMST(self):
-
-		# Key values used to pick minimum weight edge in cut
-		key = [sys.maxsize] * self.V
-		parent = [None] * self.V # Array to store constructed MST
-		# Make key 0 so that this vertex is picked as first vertex
-		key[0] = 0
-		mstSet = [False] * self.V
-
-		parent[0] = -1 # First node is always the root of
-
-		for cout in range(self.V):
-
-			# Pick the minimum distance vertex from
-			# the set of vertices not yet processed.
-			# u is always equal to src in first iteration
-			u = self.minKey(key, mstSet)
-
-			# Put the minimum distance vertex in
-			# the shortest path tree
-			mstSet[u] = True
-
-			# Update dist value of the adjacent vertices
-			# of the picked vertex only if the current
-			# distance is greater than new distance and
-			# the vertex in not in the shortest path tree
-			for v in range(self.V):
-
-				# graph[u][v] is non zero only for adjacent vertices of m
-				# mstSet[v] is false for vertices not yet included in MST
-				# Update the key only if graph[u][v] is smaller than key[v]
-				if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
-						key[v] = self.graph[u][v]
-						parent[v] = u
-
-		self.printMST(parent)
-
-g = Graph(5)
-g.graph = [ [0, 2, 0, 6, 0],
-			[2, 0, 3, 8, 5],
-			[0, 3, 0, 0, 7],
-			[6, 8, 0, 0, 9],
-			[0, 5, 7, 9, 0]]
-
-g.primMST();
+# function to schedule the jobs take 2
+# arguments array and no of jobs to schedule
 
 
+def printJobScheduling(arr, t):
+
+	# length of array
+	n = len(arr)
+
+	# Sort all jobs according to
+	# decreasing order of profit
+	for i in range(n):
+		for j in range(n - 1 - i):
+			if arr[j][2] < arr[j + 1][2]:
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+	# To keep track of free time slots
+	result = [False] * t
+
+	# To store result (Sequence of jobs)
+	job = ['-1'] * t
+
+	# Iterate through all given jobs
+	for i in range(len(arr)):
+
+		# Find a free slot for this job
+		# (Note that we start from the
+		# last possible slot)
+		for j in range(min(t - 1, arr[i][1] - 1), -1, -1):
+
+			# Free slot found
+			if result[j] is False:
+				result[j] = True
+				job[j] = arr[i][0]
+				break
+
+	# print the sequence
+	print(job)
+
+
+# Driver COde
+arr = [['a', 2, 100], # Job Array
+	['b', 1, 19],
+	['c', 2, 27],
+	['d', 1, 25],
+	['e', 3, 15]]
+
+
+print("Following is maximum profit sequence of jobs")
+
+# Function Call
+printJobScheduling(arr, 3)
